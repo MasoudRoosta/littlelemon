8327c459d2ee10d27a0f655d1c5646a0551b822c
Masoud Roosta <6masoudroosta8@gmail.com>
2023-10-24 22:46:30 +0200

    App implmentation

---
+++ Restaurant/Restaurant/RestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantApp.swift
+//  Restaurant
+//
+//  Created by Masoud Roosta on 10/24/23.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
